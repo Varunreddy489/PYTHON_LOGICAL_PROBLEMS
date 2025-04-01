ebd2045e31e39d4ac74b4d54942375129e44848f
Varunreddy <varunsannapureddy@gmail.com>
2025-04-01 17:19:06 +0530

    modified:   Logical Programs/perfect_sqaure.py

--- Logical Programs/perfect_sqaure.py
+++ Logical Programs/perfect_sqaure.py
@@ -1,23 +1,24 @@
 def perfect_square(num):
-    
-    low=1
-    high=num-1
-    
-    while(low<=high):
-        
-        mid=(low+high)//2
-    
-        if(mid*mid==num):
+
+    low = 1
+    high = num - 1
+
+    while low <= high:
+
+        mid = (low + high) // 2
+
+        if mid * mid == num:
             return True
-        
-        elif mid*mid>num:
-            
-            high=mid-1
-            
+
+        elif mid * mid > num:
+
+            high = mid - 1
+
         else:
-            low=mid+1
+            low = mid + 1
 
     return False
+
 
 print(perfect_square(20))
 print(perfect_square(25))
